d216b0a6496dd1bc0e61e7fdb9c9ea05d9c6b404
alexey-melekhov1989 <alexey-melekhov1989@yandex.ru>
2021-01-06 20:40:11 +0500

    update formatters

--- src/Formatters.php
+++ src/Formatters.php
@@ -6,15 +6,10 @@ namespace Differ\Formatters;
 
 function formatting($formatName, $diffData): string
 {
-    if ($formatName == 'stylish') {
-        return \Differ\Formatters\Stylish\formatDiff($diffData);
-    }
-
-    if ($formatName == 'plain') {
-        return \Differ\Formatters\Plain\formatDiff($diffData);
-    }
-
-    if ($formatName == 'json') {
-        return json_encode(\Differ\Formatters\Json\formatDiff($diffData), JSON_PRETTY_PRINT);
-    }
+    return match ($formatName) {
+        'stylish' => \Differ\Formatters\Stylish\formatDiff($diffData),
+        'plain' => \Differ\Formatters\Plain\formatDiff($diffData),
+        'json' => json_encode(\Differ\Formatters\Json\formatDiff($diffData), JSON_PRETTY_PRINT),
+    default => throw new \Exception("Unknown format name: {$formatName}!"),
+    };
 }
