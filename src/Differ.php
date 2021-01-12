20cb12b179f22306901a2eb3f413e6063c462f29
alexey-melekhov1989 <alexey-melekhov1989@yandex.ru>
2021-01-12 15:27:39 +0500

    update differ

--- src/Differ.php
+++ src/Differ.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Differ\Differ;
 
-use function Differ\Parsers\parseFile;
+use function Differ\Parsers\parse;
 use function Differ\Formatters\formatting;
 use function Funct\Collection\union;
 
@@ -13,48 +13,51 @@ function getContentFile(string $filePath): string
     return file_get_contents($filePath);
 }
 
+function getExtension(string $filename): string
+{
+    $array = explode(".", $filename);
+    return end($array);
+}
+
 function genDiff(string $pathFile1, string $pathFile2, $formatName = 'stylish'): string
 {
     $rawData1 = getContentFile($pathFile1);
     $rawData2 = getContentFile($pathFile2);
 
-    $firstData = parseFile($rawData1);
-    $secondData = parseFile($rawData2);
+    $firstData = parse($rawData1, getExtension($pathFile1));
+    $secondData = parse($rawData2, getExtension($pathFile1));
 
     $diffData = buildDiff($firstData, $secondData);
 
-    return trim(formatting($formatName, $diffData));
+    return formatting($formatName, $diffData);
 }
 
-function buildDiff($firstData, $secondData): array
+function buildDiff(object $firstData, object $secondData): array
 {
-    $firstData = get_object_vars($firstData);
-    $secondData = get_object_vars($secondData);
-
-    $firstKeys = array_keys($firstData);
-    $secondKeys = array_keys($secondData);
+    $firstKeys = array_keys(get_object_vars($firstData));
+    $secondKeys = array_keys(get_object_vars($secondData));
 
     $keys = union($firstKeys, $secondKeys);
     sort($keys);
 
     return array_map(function ($key) use ($firstData, $secondData) {
 
-        if (!array_key_exists($key, $secondData)) {
-            return ['key' => $key, 'status' => 'deleted', 'value' => $firstData[$key]];
+        if (!property_exists($secondData, $key)) {
+            return ['key' => $key, 'status' => 'deleted', 'value' => $firstData->$key];
         }
 
-        if (!array_key_exists($key, $firstData)) {
-            return ['key' => $key, 'status' => 'added', 'value' => $secondData[$key]];
+        if (!property_exists($firstData, $key)) {
+            return ['key' => $key, 'status' => 'added', 'value' => $secondData->$key];
         }
 
-        if (is_object($firstData[$key]) && is_object($secondData[$key])) {
-            return ['key' => $key, 'status' => 'nested', 'children' => buildDiff($firstData[$key], $secondData[$key])];
+        if (is_object($firstData->$key) && is_object($secondData->$key)) {
+            return ['key' => $key, 'status' => 'nested', 'children' => buildDiff($firstData->$key, $secondData->$key)];
         }
 
-        if ($secondData[$key] !== $firstData[$key]) {
-            return ['key' => $key,'status' => 'changed','oldValue' => $firstData[$key],'newValue' => $secondData[$key]];
+        if ($secondData->$key !== $firstData->$key) {
+            return ['key' => $key,'status' => 'changed','oldValue' => $firstData->$key,'newValue' => $secondData->$key];
         }
 
-        return ['key' => $key, 'status' => 'unchanged', 'value' => $firstData[$key]];
+        return ['key' => $key, 'status' => 'unchanged', 'value' => $firstData->$key];
     }, $keys);
 }
