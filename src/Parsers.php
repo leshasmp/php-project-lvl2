36b9343208cb531fc81be8bd54798eaf625efeb6
alexey-melekhov1989 <alexey-melekhov1989@yandex.ru>
2021-01-17 21:57:01 +0500

    update Parser

--- src/Parsers.php
+++ src/Parsers.php
@@ -6,13 +6,14 @@ namespace Differ\Parsers;
 
 use Symfony\Component\Yaml\Yaml;
 
-function parse(string $data, string $extension): object
+function parse(string $data, string $format): object
 {
-    if ($extension == 'json') {
-        return json_decode($data, false);
-    }
-
-    if ($extension == 'yml') {
-        return Yaml::parse($data, Yaml::PARSE_OBJECT_FOR_MAP);
+    switch ($format) {
+        case 'json':
+            return json_decode($data, false);
+        case 'yml':
+            return Yaml::parse($data, Yaml::PARSE_OBJECT_FOR_MAP);
+        default:
+            throw new \Exception("Unknown format item: {$format}!");
     }
 }
