2a3ebc8f4c403bffddfaa14854dbf5bcf726a65b
alexey-melekhov1989 <alexey-melekhov1989@yandex.ru>
2021-01-17 22:21:31 +0500

    update formatter json

--- src/Formatters.php
+++ src/Formatters.php
@@ -9,7 +9,7 @@ function format($formatName, $diffData): string
     return match ($formatName) {
         'stylish' => Stylish\formatDiff($diffData),
         'plain' => Plain\formatDiff($diffData),
-        'json' => json_encode(Json\formatDiff($diffData)),
+        'json' => json_encode($diffData),
     default => throw new \Exception("Unknown format name: {$formatName}!"),
     };
 }
