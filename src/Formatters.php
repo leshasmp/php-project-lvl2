c4caed0e1d4fd8638a1f2c2b67167e0a9b987c47
alexey-melekhov1989 <alexey-melekhov1989@yandex.ru>
2021-01-19 13:18:51 +0500

    return type format

--- src/Formatters.php
+++ src/Formatters.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 namespace Differ\Formatters;
 
-function format(string $formatName, array $diffData): string
+function format(string $formatName, array $diffData): string|false
 {
     switch ($formatName) {
         case 'stylish':
-            return Stylish\format($diffData) ?? '';
+            return Stylish\format($diffData);
         case 'plain':
-            return Plain\format($diffData) ?? '';
+            return Plain\format($diffData);
         case 'json':
-            return json_encode($diffData) ?? '';
+            return json_encode($diffData);
         default:
             throw new \Exception("Unknown format name: {$formatName}!");
     }
